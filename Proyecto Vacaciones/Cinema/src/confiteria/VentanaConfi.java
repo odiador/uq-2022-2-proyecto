a2af56790fbe4c8ae42560ec2ad43519aa2802a6
Juan Manuel Amador Roa <115110279+Corem05@users.noreply.github.com>
2023-03-29 22:02:45 -0500

    Update VentanaConfi.java
    - Changed the button that allows to see the cart to clear cart
    - Deleted the name displayed on the frame
    - Changed the size of the frame

--- Proyecto Vacaciones/Cinema/src/confiteria/VentanaConfi.java
+++ Proyecto Vacaciones/Cinema/src/confiteria/VentanaConfi.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 
 import javax.swing.BorderFactory;
 import javax.swing.JOptionPane;
-import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 
 import config.Constantes;
@@ -20,48 +19,49 @@ import objects.Cliente;
 import objects.ListaConfiteria;
 
 public class VentanaConfi extends Plantilla implements PagoListener {
-	private Text bPagar, bCarrito, bCancelar, tTitulo;
+	private Text bPagar, bVaciar, bCancelar;
 	private Panel inferior;
 	private SelecionConfi panelConfi;
+
 	private ListaConfiteria carrito = new ListaConfiteria();
 	private Cliente cliente;
+	private PanelPrevioConfi panelPrevio;
 
 	public VentanaConfi(Cliente c) {
 		setCliente(c);
+		panelPrevio = new PanelPrevioConfi();
+		panelPrevio.setInfo(carrito);
+		panelPrevio.setPreferredSize(300, 300);
 
 		Font f = Constantes.defaultFont.deriveFont(25f);
-		settTitulo(new Text("Nombre: " + c.getName(), f));
 		setbCancelar(new Text("Cancelar", f));
 		setbPagar(new Text("Pagar", f));
-		setbCarrito(new Text("Ver Carrito", f));
-
-		setPaneConfi(new SelecionConfi(getCarrito()));
+		setbVaciar(new Text("Vaciar Carrito", f));
+		SelecionConfi selecionConfi = new SelecionConfi(getCarrito());
+		selecionConfi.addCustomListener(() -> panelPrevio.setInfo(carrito));
+		setPaneConfi(selecionConfi);
 
 		getPanelConfi().setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-		gettTitulo().setOpaque(true);
 		getbCancelar().configAsButton(this);
 		getbPagar().configAsButton(this);
-		getbCarrito().configAsButton(this);
-
-		gettTitulo().setVerticalAlignment(SwingConstants.CENTER);
-		gettTitulo().setPreferredSize(300, 100);
+		getbVaciar().configAsButton(this);
 
 		setInferior(new Panel());
 		getInferior().setLayout(new GridLayout(1, 3));
 		getInferior().setPreferredSize(1, 40);
 
 		getInferior().add(getbPagar());
-		getInferior().add(getbCarrito());
+		getInferior().add(getbVaciar());
 		getInferior().add(getbCancelar());
 
 		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		agregarBody(gettTitulo(), BorderLayout.NORTH);
 		agregarBody(getPanelConfi(), BorderLayout.CENTER);
 		agregarBody(getInferior(), BorderLayout.SOUTH);
+		agregarBody(panelPrevio, BorderLayout.EAST);
 	}
 
 	@Override
-	public void botonPresionado (Evento e) {
+	public void botonPresionado(Evento e) {
 		super.botonPresionado(e);
 		if (e.getSource() == getbPagar()) {
 			getPanelConfi().removeAll();
@@ -75,111 +75,98 @@ public class VentanaConfi extends Plantilla implements PagoListener {
 			getInferior().setPreferredSize(0, 0);
 			getInferior().revalidate();
 		}
-		if (e.getSource() == getbCarrito()) {
-			getPanelConfi().removeAll();
-			getPanelConfi().setLayout(new BorderLayout());
-			getPanelConfi().add(new CarritoConfi(getCarrito()));
-			getPanelConfi().setBorder(null);
-			getPanelConfi().revalidate();
-			getPanelConfi().repaint();
-			getInferior().setPreferredSize(0, 0);
-			getInferior().revalidate();
+		if (e.getSource() == getbVaciar()) {
+			carrito.clear();
+			panelPrevio.setInfo(carrito);
 		}
-		if (e.getSource() == getbCancelar()) dispose();
+		if (e.getSource() == getbCancelar())
+			dispose();
 
 	}
 
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		VentanaConfi p = new VentanaConfi(new Cliente("Juan Antonio de las Nieves", 1098343206));
 		p.setVisible(true);
 	}
 
 	@Override
-	public void pagoRealizado (String infoExtra) {
+	public void pagoRealizado(String infoExtra) {
 		JOptionPane.showMessageDialog(null, "Pago realizado con éxito", "Info", JOptionPane.INFORMATION_MESSAGE);
 		botonPresionado(new Evento(getbCancelar()));
 	}
 
 	@Override
-	public void pagoNoRealizado (SQLException e) {
+	public void pagoNoRealizado(SQLException e) {
 		JOptionPane.showMessageDialog(null, "Error: " + e, "Error", JOptionPane.ERROR_MESSAGE);
 	}
 
 	@Override
-	public void volver () {
+	public void volver() {
 		getPanelConfi().revalidate();
 		getPanelConfi().repaint();
 		getInferior().setPreferredSize(1, 40);
 		getInferior().revalidate();
 	}
 
-	public Text getbPagar () {
+	public Text getbPagar() {
 		return bPagar;
 	}
 
-	public void setbPagar (Text bPagar) {
+	public void setbPagar(Text bPagar) {
 		this.bPagar = bPagar;
 	}
 
-	public Text getbCancelar () {
+	public Text getbCancelar() {
 		return bCancelar;
 	}
 
-	public void setbCancelar (Text bCancelar) {
+	public void setbCancelar(Text bCancelar) {
 		this.bCancelar = bCancelar;
 	}
 
-	public Text gettTitulo () {
-		return tTitulo;
-	}
-
-	public void settTitulo (Text tTitulo) {
-		this.tTitulo = tTitulo;
-	}
-
-	public Panel getInferior () {
+	public Panel getInferior() {
 		return inferior;
 	}
 
-	public void setInferior (Panel inferior) {
+	public void setInferior(Panel inferior) {
 		this.inferior = inferior;
 	}
 
-	public SelecionConfi getPanelConfi () {
+	public SelecionConfi getPanelConfi() {
 		return panelConfi;
 	}
 
-	public void setPaneConfi (SelecionConfi panelConfi) {
+	public void setPaneConfi(SelecionConfi panelConfi) {
 		this.panelConfi = panelConfi;
 	}
 
 	@Override
-	public void configurarTam () {
-		setSize(800, 500);
+	public void configurarTam() {
+		setSize(1100, 550);
 
 	}
 
-	public Cliente getCliente () {
+	public Cliente getCliente() {
 		return cliente;
 	}
 
-	public void setCliente (Cliente c) {
+	public void setCliente(Cliente c) {
 		this.cliente = c;
 	}
 
-	public Text getbCarrito () {
-		return bCarrito;
+	public Text getbVaciar() {
+		return bVaciar;
 	}
 
-	public void setbCarrito (Text bCarrito) {
-		this.bCarrito = bCarrito;
+	public void setbVaciar(Text bCarrito) {
+		this.bVaciar = bCarrito;
 	}
 
-	public ListaConfiteria getCarrito () {
+	public ListaConfiteria getCarrito() {
 		return carrito;
 	}
 
-	public void setCarrito (ListaConfiteria carrito) {
+	public void setCarrito(ListaConfiteria carrito) {
 		this.carrito = carrito;
 	}
 }
